456d9be528ece13ae49c2931aa6a8f5321633fb2
Mazen Abo Elanin Ahmed <mazenaboelanin@gmail.com>
2023-07-17 01:25:22 +0300

    Feat: add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TasksListComponent } from './pages/tasks-list/tasks-list.component';
+import { AddTaskComponent } from './pages/add-task/add-task.component';
+import { TaskDetailsComponent } from './pages/task-details/task-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        component: TasksListComponent
+      },
+      {
+        path: 'add-task',
+        component: AddTaskComponent
+      },
+      {
+        path: 'task-details',
+        component: TaskDetailsComponent
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
