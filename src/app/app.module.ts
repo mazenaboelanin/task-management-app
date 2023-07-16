4e4515b50952db8762f53d63088011b6f971f0f7
Mazen Abo Elanin Ahmed <mazenaboelanin@gmail.com>
2023-07-17 01:25:12 +0300

    Feat: add project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TasksListComponent } from './pages/tasks-list/tasks-list.component';
+import { TaskDetailsComponent } from './pages/task-details/task-details.component';
+import { AddTaskComponent } from './pages/add-task/add-task.component';
+import { TaskCardComponent } from './components/task-card/task-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksListComponent,
+    TaskDetailsComponent,
+    AddTaskComponent,
+    TaskCardComponent
   ],
   imports: [
     BrowserModule,
