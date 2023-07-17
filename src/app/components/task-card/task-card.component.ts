5e203cd2a65630ef35f4f517a432911d41e04a47
Mazen Abo Elanin Ahmed <mazenaboelanin@gmail.com>
2023-07-17 03:27:10 +0300

    Feat: display task data in card

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-task-card',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-card.component.scss']
 })
 export class TaskCardComponent implements OnInit {
+  @Input() tasks: any;
 
   constructor() { }
 
