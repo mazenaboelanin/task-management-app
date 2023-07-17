1f8945258adf60ccc5bd010c3e2816e478bd44ac
Mazen Abo Elanin Ahmed <mazenaboelanin@gmail.com>
2023-07-17 03:26:36 +0300

    Feat: passing tasks to task cards

--- src/app/pages/tasks-list/tasks-list.component.ts
+++ src/app/pages/tasks-list/tasks-list.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks-list.component.scss']
 })
 export class TasksListComponent implements OnInit {
+  tasks: any = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.tasks = [
+      {
+        id:1,
+        title: 'task1',
+        priority: 1,
+        date: new Date().toLocaleString(),
+        state: 'inprogress',
+        description: 'descipriton of task1'
+      },
+      {
+        id:2,
+        title: 'task2',
+        priority: 2,
+        date: new Date().toLocaleString(),
+        state: 'Todo',
+        description: 'descipriton of task2'
+      },
+      {
+        id:3,
+        title: 'task3',
+        priority: 3,
+        date: new Date().toLocaleString(),
+        state: 'Todo',
+        description: 'descipriton of task3'
+      }
+    ];
   }
 
 }
